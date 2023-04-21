86d4ae2550fdba11f15c76a737ffd959f11923ee
celso.campos <celso.campos@ideiasdinamicas.com>
2023-04-21 23:50:53 +0100

    API com AdonisJS e Postgres

---
+++ Procfile
@@ -0,0 +1,3 @@
+release: ENV_SILENT=true node ace migration
+web: ENV_SILENT=true node ace migration
+start
---
+++ app/Controllers/Http/CourseController.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const Course = use('App/Models/Course')
+
+class CourseController {
+
+  async store({ request }) {
+    const dataToCreate = request.only(['name', 'description', 'url', 'price'])
+    return await Course.create(dataToCreate)
+  }
+
+  async getAll() {
+    return await Course.all()
+  }
+
+  async show({ params }) {
+    return await Course.find(params.id)
+  }
+
+  async update({ params, request }) {
+    const course = await Course.findOrFail(params.id)
+    const dataToUpdate = request.only(['name', 'description', 'url', 'price'])
+
+    course.merge(dataToUpdate)
+    await course.save()
+
+    return course
+  }
+
+  async delete({ params }) {
+    const course = await Course.findOrFail(params.id)
+    await course.delete()
+
+    return {
+      message: 'Curso deletado com sucesso'
+    }
+  }
+}
+
+module.exports = CourseController
